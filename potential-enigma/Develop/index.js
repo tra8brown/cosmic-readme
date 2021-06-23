e665842edf6fad503f4c0a95b725aef46fccbcd4
TraBee <tra8brown@gmail.com>
2021-06-23 15:11:06 -0600

    computer restart

--- potential-enigma/Develop/index.js
+++ potential-enigma/Develop/index.js
@@ -1,25 +1,29 @@
+//09 activity 2
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
 const questions = [
-    //name of project 
-    //Description of title
-    //table of contents
-    //installation
-    //usage
-    //license
-    //contributing
-    //tests
-    //questions
-    //github username
-    //email address
+    // name of project 
+    // Description of title
+    // table of contents
+    // installation
+    // usage
+    // license
+    // contributing
+    // tests
+    // questions
+    // github username
+    // email address
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+//look at 09 activity 1.
 
 // TODO: Create a function to initialize app
 function init() {}
+// 09 activity 2 
 
 // Function call to initialize app
 init();
